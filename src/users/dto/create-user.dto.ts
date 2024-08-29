0cc4b7115bf68e224889726b28a68ca3f5a7291f
Emanuel de Oliveira <oliveirabar.emanuel@gmail.com>
2024-08-29 20:59:11 -0300

    :sparkles: feat: add class-validator and update database

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,10 @@
-export class CreateUserDto {}
+import { PickType } from '@nestjs/mapped-types';
+import { UserEntity } from '../entities/user.entity';
+
+export class CreateUserDto extends PickType(UserEntity, [
+  'nome',
+  'email',
+  'senha',
+  'fotoPerfil',
+  'enderecoID',
+]) {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const { enderecoID, ...data } = createUserDto;
+    co
     return 'This action adds a new user';
   }
 
