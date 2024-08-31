5a5cc49754f2e7f932e27e01174ed1641ebc7a55
root <root@DESKTOP-N6V918U>
2024-08-31 20:07:11 -0300

    retirando class validato da main

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import 'class-validator'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
