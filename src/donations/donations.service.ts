0266279d69fa97d9399fd5ae729ab995ef7908d6
root <root@DESKTOP-N6V918U>
2024-09-03 21:32:50 -0300

    feat: notificacao por email para o usuario de entrega concluida

--- src/donations/donations.service.ts
+++ src/donations/donations.service.ts
@@ -3,18 +3,14 @@ import { CreateDonationDto } from './dto/create-donation.dto';
 import { UpdateDonationDto } from './dto/update-donation.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { DonationEntity } from './entities/donation.entity';
-
+import transporter from 'src/nodemailer';
 @Injectable()
 export class DonationsService {
   constructor(private readonly prisma: PrismaService) {}
-  
-  async create(createDonationDto: CreateDonationDto): Promise<DonationEntity> {
-    // Use o Prisma para criar uma nova doação no banco de dados
-    
 
+  async create(createDonationDto: CreateDonationDto): Promise<DonationEntity> {
     const donation = await this.prisma.doacao.create({
       data: {
-        
         usuarioID: createDonationDto.usuarioID,
         destinatarioID: createDonationDto.destinatarioID,
         descricao: createDonationDto.descricao,
@@ -25,7 +21,7 @@ export class DonationsService {
       },
     });
 
-    return donation; 
+    return donation;
   }
 
   async findAll(): Promise<DonationEntity[]> {
@@ -34,7 +30,7 @@ export class DonationsService {
 
   async findOne(id: number): Promise<DonationEntity> {
     const donation = await this.prisma.doacao.findUnique({
-      where: { id: id },
+      where: { id },
     });
 
     if (!donation) {
@@ -73,32 +69,42 @@ export class DonationsService {
     if (!donation) {
       throw new NotFoundException(`Donation with id ${id} not found`);
     }
-
-    
   }
 
   async entregaConcluida(id: number): Promise<DonationEntity> {
     const donation = await this.prisma.doacao.findUnique({
       where: { id },
     });
-  
+
     if (!donation) {
       throw new NotFoundException(`Donation with id ${id} not found`);
     }
-  
+
     const updatedDonation = await this.prisma.doacao.update({
       where: { id },
       data: {
-        entregue: true, 
+        entregue: true,
       },
     });
-  
+
+    // Supondo que o email do usuário esteja relacionado ao ID do usuário
+    const user = await this.prisma.usuario.findUnique({
+      where: { id: donation.usuarioID },
+    });
+
+    if (user && user.email) {
+      await transporter.sendMail({
+        from: 'doarpontocom@gmail.com', 
+        to: user.email, 
+        subject: 'Entrega confirmada',
+        text: `A entrega do pedido ${id} foi confirmada com sucesso, visite o site para acompanhar as imagens de entrega.`,
+      });
+    } else {
+      throw new NotFoundException(`Email do usuário com ID ${donation.usuarioID} não encontrado`);
+    }
+
     return updatedDonation;
   }
-}
-
-
-
-
 
   
+}
