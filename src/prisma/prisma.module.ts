d376f8500e5f8ffbff1aca346cf6649e08fdb34e
root <root@DESKTOP-N6V918U>
2024-09-11 16:11:34 -0300

    fix:prisma module

---
+++ src/prisma/prisma.module.ts
@@ -0,0 +1,9 @@
+// prisma.module.ts
+import { Module } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+
+@Module({
+  providers: [PrismaService],
+  exports: [PrismaService], // Exporta o PrismaService para ser utilizado em outros módulos
+})
+export class PrismaModule {}
